ce8ec7527010805892bc8078fd1d8eb373c0fa76
Zack Bloom <zackbloom@gmail.com>
2016-04-08 14:21:35 -0400

    Add live updating

--- app.js
+++ app.js
@@ -3,10 +3,47 @@
   var options = INSTALL_OPTIONS;
   var isPreview = INSTALL_ID == "preview";
 
-  options.embedId = options.embedId || "d47y62ynnhpg";
+  options.embedId = options.embedId;
+
+  if (isPreview && !options.embedId)
+    options.embedId = "f6r6234aekhz";
 
   if (!options.embedId) {
     return;
+  }
+
+  if (isPreview){
+    var config = null;
+    var loadConfiguration = function(cb){
+      var xhr = new XMLHttpRequest;
+      xhr.open('GET', "https://customer.api.drift.com/embeds/" + options.embedId, true);
+      xhr.onload = function(){
+        cb(JSON.parse(xhr.response).configuration);
+      };
+      xhr.onerror = function(err){
+        console.error("Error loading drift config", err);
+      };
+      xhr.send();
+    }
+
+    var writeConfig = function(){
+      if (config && window.driftt && window.driftt.__overrideEmbedConfiguration__)
+        driftt.__overrideEmbedConfiguration__(config);
+    }
+
+    INSTALL_SCOPE.setOptions = function(opts){
+      options = opts;
+
+      config.organizationName = options.orgName;
+      config.theme.backgroundColor = options.color.replace(/^#/, '');
+
+      writeConfig()
+      console.log(options.logo);
+    }
+
+    loadConfiguration(function(conf){
+      config = conf;
+    });
   }
 
   (function(t) {
